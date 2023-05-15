984035427f08b78017380f19571f214c03123bf0
Louis Brochard <louisbrochard@macbook-air-de-louis-4.home>
2023-05-15 14:08:11 +0200

    16mai2023

---
+++ srcs/ft_strlcpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: louisbrochard <louisbrochard@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 19:13:47 by lbrochar          #+#    #+#             */
+/*   Updated: 2023/05/15 13:28:51 by louisbrocha      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (n == 0)
+		return (ft_strlen(src));
+	while ((i < (n - 1)) && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
